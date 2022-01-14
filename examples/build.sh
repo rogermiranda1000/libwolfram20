3867884ce8aceb011014858d121a0c16f022b202
rogermiranda1000 <rogermirandaperez@hotmail.com>
2022-01-14 01:36:17 +0100

    Fix: app token

--- examples/build.sh
+++ examples/build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-echo "#define APP_ID \"$APP_ID\"" > "$PWD/examples"
+echo "#define APP_ID \"$APP_ID\"" > "$PWD/examples/secrets.h"
 for example in `ls -d $PWD/examples/*/`; do # POV libwolfram20 root directory
 	cd "$example"
-	cp ../secrets.h .
+	cp ../secrets.h .; ret=$?
 	
-	mkdir "build" 2>/dev/null
-	cd "build"
-	
-	cmake ..
-	make
+	if [ $? -eq 0 ]; then
+		mkdir "build" 2>/dev/null
+		cd "build"
+		
+		cmake ..
+		make
+	fi
 done
