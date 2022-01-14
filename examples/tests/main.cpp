0cc6d70ff0d3d980fc12d19cb642c09a6e835d9c
rogermiranda1000 <rogermirandaperez@hotmail.com>
2022-01-14 17:53:27 +0100

    Feature: all the tests

--- examples/tests/main.cpp
+++ examples/tests/main.cpp
@@ -51,6 +51,40 @@ TEST(solve, PrevistedOutput) {
 	free(result);
 }
 
+TEST(solve2d, PrevistedOutput) {
+	ForkedPipeInfo fork_pipe;
+	char *command = getExamplePath("solve2d");
+	
+	ASSERT_EQ(executeProgramLineWithPipe(&fork_pipe, &command, NULL, NULL), 0) << "[e] Error executing 'solve2d' program" << std::endl << flushFd(fork_pipe);
+	char *result;
+	discard(fdPipeInfo(fork_pipe, 1), '\n'); // discard the first line (Searching ...)
+	readUntil(fdPipeInfo(fork_pipe, 1), &result, ' '); // get the result
+	
+	// Expect two strings not to be equal.
+	EXPECT_STREQ("Graph:", result) << "[w] Unexpected result: " << result;
+	freeForkedPipeInfo(&fork_pipe);
+	free(result);
+}
+
+TEST(solve_exhaustive, PrevistedOutput) {
+	ForkedPipeInfo fork_pipe;
+	char *command = getExamplePath("solve-exhaustive");
+	
+	ASSERT_EQ(executeProgramLineWithPipe(&fork_pipe, &command, NULL, NULL), 0) << "[e] Error executing 'solve-exhaustive' program" << std::endl << flushFd(fork_pipe);
+	char *result, *result2;
+	discard(fdPipeInfo(fork_pipe, 1), '\n'); // discard the first line (Searching ...)
+	readUntil(fdPipeInfo(fork_pipe, 1), &result, '\n'); // get the result
+	discard(fdPipeInfo(fork_pipe, 1), '\n'); // discard the second line (Searching ...)
+	readUntil(fdPipeInfo(fork_pipe, 1), &result2, ' '); // get the result
+	
+	// Expect two strings not to be equal.
+	EXPECT_STREQ("Keep searching!", result) << "[w] Unexpected result: " << result;
+	EXPECT_STREQ("Solution:", result2) << "[w] Unexpected result: " << result2;
+	freeForkedPipeInfo(&fork_pipe);
+	free(result);
+	free(result2);
+}
+
 int main(int argc, char *argv[], char *envp[]) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
